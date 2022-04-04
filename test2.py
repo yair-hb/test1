501594eb2281c341264ed71959da8e8e0ae82501
yair-hb <aythb@hotmail.com>
2022-04-04 14:38:26 -0500

    otro commit de prueba

--- test2.py
+++ test2.py
@@ -1,1 +1,3 @@
 #esta es una linea de comentario en el archivo 
+color = input("Cual es tu color favorito?")
+print ("tu color favorito es el "+ color)
--- test3.py
+++ test3.py
@@ -1,1 +1,3 @@
 #esta son unas lineas cometadas en la parte princiapl del archivo 
+print ("este es un mensaje para que se lea desde el terminal")
+
